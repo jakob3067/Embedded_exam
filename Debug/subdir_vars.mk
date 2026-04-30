65712823dbf85b1b7fe7e3386321a7ea3ea6777e
Jakob <jakob3067.tietgen@gmail.com>
2026-04-30 14:01:27 +0200

    Alt er sat op med FREE RTOS

--- Debug/subdir_vars.mk
+++ Debug/subdir_vars.mk
@@ -6,28 +6,78 @@
 CMD_SRCS += \
 ../tm4c123gh6pm.cmd 
 
+ASM_SRCS += \
+../portasm.asm 
+
 C_SRCS += \
+../heap_2.c \
+../list.c \
 ../main.c \
-../tm4c123gh6pm_startup_ccs.c 
+../port.c \
+../queue.c \
+../startup_frt.c \
+../systick_frt.c \
+../tasks.c 
 
 C_DEPS += \
+./heap_2.d \
+./list.d \
 ./main.d \
-./tm4c123gh6pm_startup_ccs.d 
+./port.d \
+./queue.d \
+./startup_frt.d \
+./systick_frt.d \
+./tasks.d 
 
 OBJS += \
+./heap_2.obj \
+./list.obj \
 ./main.obj \
-./tm4c123gh6pm_startup_ccs.obj 
+./port.obj \
+./portasm.obj \
+./queue.obj \
+./startup_frt.obj \
+./systick_frt.obj \
+./tasks.obj 
+
+ASM_DEPS += \
+./portasm.d 
 
 OBJS__QUOTED += \
+"heap_2.obj" \
+"list.obj" \
 "main.obj" \
-"tm4c123gh6pm_startup_ccs.obj" 
+"port.obj" \
+"portasm.obj" \
+"queue.obj" \
+"startup_frt.obj" \
+"systick_frt.obj" \
+"tasks.obj" 
 
 C_DEPS__QUOTED += \
+"heap_2.d" \
+"list.d" \
 "main.d" \
-"tm4c123gh6pm_startup_ccs.d" 
+"port.d" \
+"queue.d" \
+"startup_frt.d" \
+"systick_frt.d" \
+"tasks.d" 
+
+ASM_DEPS__QUOTED += \
+"portasm.d" 
 
 C_SRCS__QUOTED += \
+"../heap_2.c" \
+"../list.c" \
 "../main.c" \
-"../tm4c123gh6pm_startup_ccs.c" 
+"../port.c" \
+"../queue.c" \
+"../startup_frt.c" \
+"../systick_frt.c" \
+"../tasks.c" 
+
+ASM_SRCS__QUOTED += \
+"../portasm.asm" 
 
 
