fbc82a5cae8a3a28919410a3e0ab28a352a5dcc9
PovlBonde <povljunior5@gmail.com>
2026-05-02 14:33:08 +0100

    Test: Main Stucture
    Testing GitHub / Code Composer Studio / VS Code setup

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
+#include <std.int.h>
+#include <stdbool.h>
 
+#define SYSCTL_RCGCGPIO_R      (*((volatile uint32_t *)0x400FE608))
+#define GPIO_PORTB_DATA_R      (*((volatile uint32_t *)0x400053FC))
+#define GPIO_PORTB_DIR_R       (*((volatile uint32_t *)0x40005400))
+#define GPIO_PORTB_DEN_R       (*((volatile uint32_t *)0x4000551C))
 
-/**
- * main.c
- */
+void Init_LCD(void);
+void LCD_WriteString(char *str);
+
 int main(void)
 {
+
+
 	return 0;
+}
+
+void Init_LCD(void)
+{
+
+}
+
+void LCD_WriteString(char *str)
+{
+
 }
