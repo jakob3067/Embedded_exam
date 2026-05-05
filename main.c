7b10a8f91c7e565106e3185be30c21165b5304e6
PovlBonde <povljunior5@gmail.com>
2026-05-05 12:00:26 +0100

    Test: Fresh start

--- main.c
+++ main.c
@@ -1,48 +1,11 @@
-
-
-/**
- * main.c
- */
 #include <stdint.h>
 #include "tm4c123gh6pm.h"
-#include "emp_type.h"
 #include "systick_frt.h"
 #include "FreeRTOS.h"
-#include "task.h"
-#include "status_led.h"
-#include "lcd.h"
 
-#define USERTASK_STACK_SIZE configMINIMAL_STACK_SIZE
-#define IDLE_PRIO 0
-#define LOW_PRIO  1
-#define MED_PRIO  2
-#define HIGH_PRIO 3
-
-static void setupHardware(void)
-/*****************************************************************************
-*   Input    :  -
-*   Output   :  -
-*   Function :
-*****************************************************************************/
-{
-  // TODO: Put hardware configuration and initialisation in here
-
-  // Warning: If you do not initialize the hardware clock, the timings will be inaccurate
-  init_systick();
-  status_led_init();
-  init_gpio();
-}
 
 int main(void)
 {
-    setupHardware();
-    init_lcd();
-    print_lcd("Welcome");
-
-    xTaskCreate( status_led_task, "Status_led", USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
-    xTaskCreate( lcd_task, "LCD", USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
-
-    vTaskStartScheduler();
 
 	while(1){
 	    // Write to the LCD
