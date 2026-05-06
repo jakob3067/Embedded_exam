18316b1c94bbc5abba2b8182dae4bad3643b77a3
PovlBonde <povljunior5@gmail.com>
2026-05-06 01:38:50 +0100

    Test: Gave up for tonight
    Kill me

---
+++ Debug/FreeRTOS_Source/queue.d
@@ -0,0 +1,81 @@
+# FIXED
+
+FreeRTOS_Source/queue.obj: C:/Users/povlj/Downloads/frt10/src/queue.c
+FreeRTOS_Source/queue.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h
+FreeRTOS_Source/queue.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+FreeRTOS_Source/queue.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+FreeRTOS_Source/queue.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+FreeRTOS_Source/queue.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h
+FreeRTOS_Source/queue.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h
+FreeRTOS_Source/queue.obj: C:/Users/povlj/Downloads/frt10/inc/FreeRTOS.h
+FreeRTOS_Source/queue.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h
+FreeRTOS_Source/queue.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+FreeRTOS_Source/queue.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+FreeRTOS_Source/queue.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+FreeRTOS_Source/queue.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+FreeRTOS_Source/queue.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+FreeRTOS_Source/queue.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+FreeRTOS_Source/queue.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+FreeRTOS_Source/queue.obj: C:/Users/povlj/Downloads/frt10/port/TivaM4/FreeRTOSConfig.h
+FreeRTOS_Source/queue.obj: C:/Users/povlj/Downloads/frt10/inc/projdefs.h
+FreeRTOS_Source/queue.obj: C:/Users/povlj/Downloads/frt10/inc/portable.h
+FreeRTOS_Source/queue.obj: C:/Users/povlj/Downloads/frt10/inc/deprecated_definitions.h
+FreeRTOS_Source/queue.obj: C:/Users/povlj/Downloads/frt10/port/TivaM4/portmacro.h
+FreeRTOS_Source/queue.obj: C:/Users/povlj/Downloads/frt10/inc/mpu_wrappers.h
+FreeRTOS_Source/queue.obj: C:/Users/povlj/Downloads/frt10/inc/task.h
+FreeRTOS_Source/queue.obj: C:/Users/povlj/Downloads/frt10/inc/list.h
+FreeRTOS_Source/queue.obj: C:/Users/povlj/Documents/GitHub/Embedded_exam/queue.h
+FreeRTOS_Source/queue.obj: C:/Users/povlj/Documents/GitHub/Embedded_exam/emp_type.h
+
+C:/Users/povlj/Downloads/frt10/src/queue.c:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h:
+
+C:/Users/povlj/Downloads/frt10/inc/FreeRTOS.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+C:/Users/povlj/Downloads/frt10/port/TivaM4/FreeRTOSConfig.h:
+
+C:/Users/povlj/Downloads/frt10/inc/projdefs.h:
+
+C:/Users/povlj/Downloads/frt10/inc/portable.h:
+
+C:/Users/povlj/Downloads/frt10/inc/deprecated_definitions.h:
+
+C:/Users/povlj/Downloads/frt10/port/TivaM4/portmacro.h:
+
+C:/Users/povlj/Downloads/frt10/inc/mpu_wrappers.h:
+
+C:/Users/povlj/Downloads/frt10/inc/task.h:
+
+C:/Users/povlj/Downloads/frt10/inc/list.h:
+
+C:/Users/povlj/Documents/GitHub/Embedded_exam/queue.h:
+
+C:/Users/povlj/Documents/GitHub/Embedded_exam/emp_type.h:
+
---
+++ Debug/FreeRTOS_Source/subdir_vars.mk
@@ -0,0 +1,74 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+ASM_SRCS += \
+C:/Users/povlj/Downloads/frt10/port/TivaM4/portasm.asm 
+
+C_SRCS += \
+C:/Users/povlj/Downloads/frt10/port/MemMang/heap_2.c \
+C:/Users/povlj/Downloads/frt10/src/list.c \
+C:/Users/povlj/Downloads/frt10/port/TivaM4/port.c \
+C:/Users/povlj/Downloads/frt10/src/queue.c \
+C:/Users/povlj/Downloads/frt10/port/TivaM4/startup_frt.c \
+C:/Users/povlj/Downloads/frt10/port/TivaM4/systick_frt.c 
+
+C_DEPS += \
+./FreeRTOS_Source/heap_2.d \
+./FreeRTOS_Source/list.d \
+./FreeRTOS_Source/port.d \
+./FreeRTOS_Source/queue.d \
+./FreeRTOS_Source/startup_frt.d \
+./FreeRTOS_Source/systick_frt.d \
+./FreeRTOS_Source/tasks.d 
+
+OBJS += \
+./FreeRTOS_Source/heap_2.obj \
+./FreeRTOS_Source/list.obj \
+./FreeRTOS_Source/port.obj \
+./FreeRTOS_Source/portasm.obj \
+./FreeRTOS_Source/queue.obj \
+./FreeRTOS_Source/startup_frt.obj \
+./FreeRTOS_Source/systick_frt.obj \
+./FreeRTOS_Source/tasks.obj 
+
+ASM_DEPS += \
+./FreeRTOS_Source/portasm.d 
+
+OBJS__QUOTED += \
+"FreeRTOS_Source\heap_2.obj" \
+"FreeRTOS_Source\list.obj" \
+"FreeRTOS_Source\port.obj" \
+"FreeRTOS_Source\portasm.obj" \
+"FreeRTOS_Source\queue.obj" \
+"FreeRTOS_Source\startup_frt.obj" \
+"FreeRTOS_Source\systick_frt.obj" \
+"FreeRTOS_Source\tasks.obj" 
+
+C_DEPS__QUOTED += \
+"FreeRTOS_Source\heap_2.d" \
+"FreeRTOS_Source\list.d" \
+"FreeRTOS_Source\port.d" \
+"FreeRTOS_Source\queue.d" \
+"FreeRTOS_Source\startup_frt.d" \
+"FreeRTOS_Source\systick_frt.d" \
+"FreeRTOS_Source\tasks.d" 
+
+ASM_DEPS__QUOTED += \
+"FreeRTOS_Source\portasm.d" 
+
+C_SRCS__QUOTED += \
+"C:/Users/povlj/Downloads/frt10/port/MemMang/heap_2.c" \
+"C:/Users/povlj/Downloads/frt10/src/list.c" \
+"C:/Users/povlj/Downloads/frt10/port/TivaM4/port.c" \
+"C:/Users/povlj/Downloads/frt10/src/queue.c" \
+"C:/Users/povlj/Downloads/frt10/port/TivaM4/startup_frt.c" \
+"C:/Users/povlj/Downloads/frt10/port/TivaM4/systick_frt.c" 
+
+ASM_SRCS__QUOTED += \
+"C:/Users/povlj/Downloads/frt10/port/TivaM4/portasm.asm" 
+
+
